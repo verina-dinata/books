5cbfdd5a3ca1a798dca47db634822a89818aab81
Verina Ong Dinata <verina.od1997@gmail.com>
2022-11-25 22:30:53 +0800

    add book edit feature

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,6 +1,23 @@
-function BookEdit() {
+import { useState } from "react";
+
+function BookEdit({ book }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(title);
+  }
+
   return (
-    <div>BookEdit</div>
+    <form className="book-edit" onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
   );
 
 }
