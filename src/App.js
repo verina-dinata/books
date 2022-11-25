079c0608d59db834e16cda639fb4372306ae38e6
Verina Ong Dinata <verina.od1997@gmail.com>
2022-11-25 21:56:47 +0800

    create new array of books

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log(title);
+    const updatedBooks = [
+      ...books,
+      {
+        id: Math.round(Math.random() * 999),
+        title
+       }
+    ];
+    setBooks(updatedBooks);
   };
 
   return (
     <div>
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
   );
