13ca14f00f932c8f11dad09514901fb2299a5483
Verina Ong Dinata <verina.od1997@gmail.com>
2022-11-26 14:55:32 +0800

    add pictures for books

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -24,7 +24,8 @@ function BookShow({ book, onDelete, onEdit }) {
 
   return (
     <div className="book-show">
-      {content}
+      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`}/>
+      <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
           Edit
